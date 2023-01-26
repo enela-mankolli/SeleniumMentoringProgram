9813c40a113ef146d71a2f4b550e0d24c79d65d8
Enela Mankolli <enelamankolli@ritech.co>
2023-01-26 19:09:42 +0100

    -implemented a new test to buy some other products and check the process until the final step.

--- src/test/java/training/selenium/tests/OnlinePurchaseTest.java
+++ src/test/java/training/selenium/tests/OnlinePurchaseTest.java
@@ -63,4 +63,38 @@ public class OnlinePurchaseTest {
         checkoutCompletePage.validateMessage("THANK YOU FOR YOUR ORDER");
     }
 
+    @Test
+    public void buyAnother2Products() {
+        //1- login
+        loginPage.login("problem_user", "secret_sauce");
+        productPage.validateURL("https://www.saucedemo.com/inventory.html");
+
+        //2- add products to cart
+        productPage.addToCardProducts();
+
+        //3- check number of chosen products
+        productPage.validateNumberOfChosenProducts(2);
+        productPage.clickShoppingCardBadge();
+        yourCartPage.validateURL("https://www.saucedemo.com/cart.html");
+
+        //4- validate products
+        yourCartPage.validateProduct1Name("Sauce Labs Backpack");
+        yourCartPage.validateProduct2Name("Sauce Labs Bike Light");
+        yourCartPage.clickCheckoutButton();
+        checkoutStepOnePage.validateURL("https://www.saucedemo.com/checkout-step-one.html");
+
+        //5- CHECKOUT: YOUR INFORMATION
+        checkoutStepOnePage.completeForm("Enela", "Mankolli", "1001");
+        checkoutStepOnePage.clickContinueButton();
+        checkoutStepTwoPage.validateURL("https://www.saucedemo.com/checkout-step-two.html");
+
+        //6- CHECKOUT: OVERVIEW
+        checkoutStepTwoPage.validateTotalPrice(43.18);
+        checkoutStepTwoPage.clickFinishButton();
+
+        //7- CHECKOUT: COMPLETE!
+        checkoutCompletePage.validateURL("https://www.saucedemo.com/checkout-complete.html");
+        checkoutCompletePage.validateMessage("THANK YOU FOR YOUR ORDER");
+    }
+
 }
