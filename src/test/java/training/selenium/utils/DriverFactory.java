3231d65172100d6f8d1527fb827a75a28b94f86f
Enela Mankolli <enelamankolli@ritech.co>
2023-02-07 18:58:20 +0100

    Providing different type of browsers to execute test cases.

--- src/test/java/training/selenium/utils/DriverFactory.java
+++ src/test/java/training/selenium/utils/DriverFactory.java
@@ -2,21 +2,36 @@ package training.selenium.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
+import java.sql.Driver;
 import java.time.Duration;
 
 public class DriverFactory {
 
-    private static WebDriver driver = initDriver();
+    private static WebDriver driver = initDriver("edge");
 
     public static WebDriver getDriver() {
         return driver;
     }
 
-    private static WebDriver initDriver() {
-       // System.setProperty("webdriver.chrome.driver", "C:\\Users\\ritech\\Downloads\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ritech\\Desktop\\chromedriver.exe");
-        driver = new ChromeDriver();
+    private static WebDriver initDriver(String browserType) {
+        if (browserType.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browserType.equals("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver.exe");
+            FirefoxOptions options = new FirefoxOptions();
+            options.setBinary("C:\\Program Files\\Mozilla Firefox\\Firefox.exe");
+            driver = new FirefoxDriver(options);
+
+        } else if (browserType.equals("edge")) {
+            System.setProperty("webdriver.edge.driver", "src/test/resources/drivers/msedgedriver.exe");
+            driver = new EdgeDriver();
+
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return driver;
